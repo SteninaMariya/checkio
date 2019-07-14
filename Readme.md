addd546bb742a5462a330ff23bdbd632b0f353e5
Mariya <mmedvednikova@gmail.com>
2019-07-14 21:31:23 +0400

    all the same solution from checkio

---
+++ Readme.md
@@ -0,0 +1,24 @@
+Что пытались сделать ребята из checkio
+
+они почему-то думают, что у всех линукс!!!
+
+поэтому при запуске init вида
+
+checkio init median checkio_solutions/median.py
+
+создается файлик с заголовком
+
+#!/usr/bin/env checkio --domain=py run all-the-same
+
+что означает следующее. Если в баше запустить команду
+
+./all_the_same.py --check
+
+то выполнится примерно слудующее
+
+checkio --domain=py run all-the-same all_the_same.py --check
+
+
+Поскольку в винде все это не работает, то надо запустить такую команду руками
+
+checkio --domain=py run all-the-same all_the_same.py --check
---
+++ all_the_same.py
@@ -0,0 +1,25 @@
+from typing import List, Any
+
+
+def all_the_same(elements: List[Any]) -> bool:
+    # your code here
+    if len(elements) <= 1:
+        return True
+    first_element: Any = elements[0]
+    for element in elements[1:]:
+        if element != first_element:
+            return False
+    return True
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(all_the_same([1, 1, 1]))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert all_the_same([1, 1, 1]) == True
+    assert all_the_same([1, 2, 1]) == False
+    assert all_the_same(['a', 'a', 'a']) == True
+    assert all_the_same([]) == True
+    assert all_the_same([1]) == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
